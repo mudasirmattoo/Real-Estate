f54a8bf0b4c3833465cd5517487cfcdb312036f2
mudasirmattoo <mudasirmmattoo@gmail.com>
2024-04-30 16:39:18 +0530

    complete sign up page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react';
-import { FaBars, FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-
+import { useState } from "react";
+import { FaBars, FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,34 +10,53 @@ export default function Header() {
   };
 
   return (
-    <header className='bg-black sticky top-0 p-2 sm:p-5 opacity-95 mx-auto shadow-lg shadow-violet-600'>
-      <div className='flex justify-between items-center relative'>
+    <header className="bg-black sticky top-0 p-2 sm:p-5 opacity-95 mx-auto shadow-lg shadow-violet-600">
+      <div className="flex justify-between items-center relative">
         <Link to="/">
-        <h1 className='text-xl sm:text-5xl font-bold sm:font-extrabold'>
-          <span className='text-violet-600'>Urban</span>
-          <span className='text-violet-400'>Nest</span>
-        </h1>
+          <h1 className="text-xl sm:text-5xl font-bold sm:font-extrabold">
+            <span className="text-violet-600">Urban</span>
+            <span className="text-violet-400">Nest</span>
+          </h1>
         </Link>
-        <form className='bg-gray-800 rounded-lg p-3 flex items-center'>
-          <input type='text' placeholder='Search ...' className='bg-transparent outline-none text-white w-44 sm:w-96' />
-          <FaSearch className='text-violet-600' />
+        <form className="bg-gray-800 rounded-lg p-3 flex items-center">
+          <input
+            type="text"
+            placeholder="Search ..."
+            className="bg-transparent outline-none text-white w-44 sm:w-96"
+          />
+          <FaSearch className="text-violet-600" />
         </form>
         <div className="sm:hidden">
-          <FaBars className="text-violet-600 font-bold text-2xl cursor-pointer" onClick={toggleMenu} />
+          <FaBars
+            className="text-violet-600 font-bold text-2xl cursor-pointer"
+            onClick={toggleMenu}
+          />
           {isMenuOpen && (
             <ul className="absolute bg-black text-violet-600 font-bold py-2 px-4 rounded-b-lg right-0 mt-5 w-full opacity-90">
-               <Link to='/'><li className="text-right">Home</li></Link>
-              <Link to ='/about'><li className="text-right">About</li></Link>
-              <Link to='/sign-in'><li className="text-right">Sign In</li></Link>
+              <Link to="/">
+                <li className="text-right">Home</li>
+              </Link>
+              <Link to="/about">
+                <li className="text-right">About</li>
+              </Link>
+              <Link to="/sign-in">
+                <li className="text-right">Sign In</li>
+              </Link>
             </ul>
           )}
         </div>
         <ul className="hidden sm:flex space-x-10 text-2xl text-violet-600 ">
-          <Link to='/'><li className='hover:text-violet-300'>Home</li></Link>
-          <Link to='/about'><li className='hover:text-violet-300'>About</li></Link>
-          <Link to='/sign-in'><li className='hover:text-violet-300'>Sign In</li></Link>
+          <Link to="/">
+            <li className="hover:text-violet-300">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="hover:text-violet-300">About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="hover:text-violet-300">Sign In</li>
+          </Link>
         </ul>
       </div>
     </header>
-  )
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,54 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData,setFormData] = useState({});
+  const [error,setError] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //prevent refresh
+
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup',
+      {
+        method: 'POST',    
+        headers: {
+          'content-Type':'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      const data = await res.json();
+      if(data.success === false){
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+   
+  };
+
   return (
     <div className="my-20">
       <h1 className="text-4xl font-bold sm:text-5xl text-white my-10 text-center">
         Sign Up
       </h1>
-      <form
+      <form onSubmit = { handleSubmit }
         action=""
         className="flex flex-col w-3/4 sm:w-1/3 mx-auto justify-center gap-5"
       >
@@ -14,25 +56,25 @@ export default function SignUp() {
           type="text"
           placeholder="Username"
           id="username"
-          className="border rounded outline-none p-3 text-center"
+          className="border rounded outline-none p-3 text-center" onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
-          className="border rounded outline-none p-3 text-center"
+          className="border rounded outline-none p-3 text-center" onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
-          className="border rounded outline-none p-3 text-center"
+          className="border rounded outline-none p-3 text-center" onChange={handleChange}
         />
         <button
-          type="submit"
+          type="submit" disabled={loading} 
           className="text-2xl font-bold sm:text-3xl text-white p-3 my-5 text-center border-violet-600 rounded bg-violet-600 hover:bg-violet-400 hover:text-black disabled:opacity-80"
         >
-          SIGN UP
+          {loading ? 'Loading ...' : 'SIGN UP' }
         </button>
       </form>
       <div className="flex  gap-3 justify-center mt-5">
@@ -43,6 +85,7 @@ export default function SignUp() {
           </span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5 text-center'>{error}</p>}
     </div>
   );
 }
